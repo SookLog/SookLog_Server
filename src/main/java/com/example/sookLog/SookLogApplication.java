bd5fdf6610e73e4d9c2e16ce059a3fb6f2efec3a
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-11-04 12:21:36 +0900

    initial commit

---
+++ src/main/java/com/example/sookLog/SookLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.sookLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SookLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SookLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sookLog/SookLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sookLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SookLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
