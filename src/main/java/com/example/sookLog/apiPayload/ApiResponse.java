53d11be96d3df6236f93bd97409337854b525d4e
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-11-09 16:14:43 +0900

    feat: 성공 응답 메서드 추가 #2

--- src/main/java/com/example/sookLog/apiPayload/ApiResponse.java
+++ src/main/java/com/example/sookLog/apiPayload/ApiResponse.java
@@ -1,5 +1,7 @@
 package com.example.sookLog.apiPayload;
 
+import com.example.sookLog.apiPayload.code.BaseCode;
+import com.example.sookLog.apiPayload.code.status.SuccessStatus;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
@@ -19,6 +21,16 @@ public class ApiResponse<T> {
 	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private T result;
 
+	// 성공한 경우 응답 생성
+	public static <T> ApiResponse<T> onSuccess(T result){
+		return new ApiResponse<>(true, SuccessStatus._OK.getCode() , SuccessStatus._OK.getMessage(), result);
+	}
+
+	public static <T> ApiResponse<T> of(BaseCode code, T result){
+		return new ApiResponse<>(true, code.getReasonHttpStatus().getCode() , code.getReasonHttpStatus().getMessage(), result);
+	}
+
+	// 실패한 경우 응답 생성
 	public static <T> ApiResponse<T> onFailure(String code, String message, T data) {
 		return new ApiResponse<>(true, code, message, data);
 	}
