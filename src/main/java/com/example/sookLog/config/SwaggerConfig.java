a5ac6433e6a20d02c25a2aa218272606b9846e94
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-11-18 15:51:23 +0900

    feat: swagger 설정 #10

---
+++ src/main/java/com/example/sookLog/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.sookLog.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI SwaggerAPI() {
+		return new OpenAPI()
+			.components(new Components())
+			.info(apiInfo());
+
+	}
+
+	private Info apiInfo(){
+		return new Info()
+			.title("SookLog Swagger")
+			.description("SookLog Server API 명세서")
+			.version("1.0.0");
+	}
+}
