5fe9d864a99c4e189543a5c0ea758b29afed1724
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-11-30 22:06:47 +0900

    chore: 리다이렉트 url 수정

--- src/main/java/com/example/sookLog/apiPayload/exception/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/example/sookLog/apiPayload/exception/handler/OAuth2SuccessHandler.java
@@ -19,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
 	private final TokenProvider tokenProvider;
-	private static final String FRONTEND_URL = "myapp://home";
+	private static final String URI = "/test";
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -29,7 +29,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 		tokenProvider.generateRefreshToken(authentication, accessToken);
 
 		// 토큰 전달을 위한 redirect
-		String redirectUrl = UriComponentsBuilder.fromUriString(FRONTEND_URL)
+		String redirectUrl = UriComponentsBuilder.fromUriString(URI)
 			.queryParam("accessToken", accessToken)
 			.build().toUriString();
 
